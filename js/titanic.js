4a6acc68e394223aad6397fa18e81718542b37fc
David Wilson <mrdavidjwilson@gmail.com>
2015-10-10 16:32:40 -0700

    First drawing of all circles

---
+++ js/titanic.js
@@ -0,0 +1,55 @@
+     function draw(data) {
+
+          "use strict";
+          var margin = 75,
+              width = 1400 - margin,
+              height = 600 - margin;
+
+          var radius = 10;
+          var num_per_row = 30;
+
+          var surv_color = "blue";
+          var dead_color = "red";
+          d3.select("body")
+            .append("h2")
+            .text("Would you have survived on the Titanic? ")
+
+          var svg = d3.select("body")
+            .append("svg")
+              .attr("width", width + margin)
+              .attr("height", height + margin)
+            .append('g')
+                .attr('class','chart');
+
+          d3.select("svg")
+            .selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle");
+
+          
+
+          // Create x-axis scale mapping dates -> pixels
+          var x_scale = d3.time.scale()
+            .range([margin, width])
+            .domain([0,400]);
+
+          
+
+
+          d3.selectAll("circle")
+            .attr("cx", function(d,i) {
+                return 10 + 20 * (i % num_per_row);
+            })
+            .attr("cy", function(d,i) {
+                return 10 + 20 * Math.floor(i / num_per_row);
+            })
+            .attr("r", radius)
+            .attr("class", function(d){
+              if (d['Survived'] == 1) {
+                return 'survived';
+              } else if (d['Survived'] == 0) {
+                return 'dead';
+              }
+            });
+        };
